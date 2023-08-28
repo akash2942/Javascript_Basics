01991516c946e2d8ef464b630093e06c24e165de
Akash <ankitsri66610@gmail.com>
2023-08-28 11:30:54 +0530

    Variables Scope

---
+++ Basics/variables.js
@@ -0,0 +1,11 @@
+a = 20; // Global scope
+var b = 30; // Here, global scope but if in a fn. then function scope
+let c = 40; // Block scope
+d = 50;
+let e = [1,2,3,4,5];
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
