3d6732fb5ee9faa5aca1e98abd61600c1c9de96f
Akash <ankitsri66610@gmail.com>
2023-08-28 14:57:03 +0530

    Updated counter1

---
+++ DOM Manipulation/counter2/first.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+	document.querySelector('button').onclick = count;
+});
+
+let a = 0;
+
+function count() {
+	a++;
+	document.querySelector('h1').innerHTML = a;
+
+	if(a % 10 == 0) {
+		alert(`Counter is at ${a}`);
+	}
+}
