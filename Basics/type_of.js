5de505d8c90dce3c715f23a24c75d7b06755f26f
Akash <ankitsri66610@gmail.com>
2023-08-28 11:31:43 +0530

    typeof function

---
+++ Basics/type_of.js
@@ -0,0 +1,17 @@
+var a = 10;
+console.log(typeof a);
+
+a = true;
+console.log(typeof a);
+
+a = "banana";
+console.log(typeof a);
+
+a = null;
+console.log(typeof a);
+
+a = Infinity;
+console.log(typeof a);
+
+a = undefined;
+console.log(typeof a);
